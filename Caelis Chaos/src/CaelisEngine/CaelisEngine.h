d1b5cebf21ef5e2463cd90c28c1b393b14aabd8a
Tobix192 <tobiasbersia@gmail.com>
2025-05-08 22:17:34 -0300

    Refactor begins

---
+++ Caelis Chaos/src/CaelisEngine/CaelisEngine.h
@@ -0,0 +1,16 @@
+#ifndef CAELISENGINE_H
+#define CAELISENGINE_H
+
+#define VERSION_STRING "0.1.0"
+
+/*
+Tobi Console Game Engine
+
+Copyright (c) Tobias Bersia
+
+All rights reserved.
+*/
+
+
+
+#endif#pragma once
