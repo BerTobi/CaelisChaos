e88bcaf2458e74656c6ec5448a9fe188c589b53d
Gonzalo Heredia <gonzalo.heredia.gh8@gmail.com>
2025-05-17 18:31:17 -0300

    function create window done

--- Caelis Chaos/src/CaelisEngine/CaelisEngine.h
+++ Caelis Chaos/src/CaelisEngine/CaelisEngine.h
@@ -13,8 +13,11 @@ All rights reserved.
 
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <SDL3_image/SDL_image.h>
+#include <SDL3_ttf/SDL_ttf.h>
 #include <stdio.h>
 #include <string>
+#include <Windows.h>
 
 class CaelisEngine
 {
@@ -26,6 +29,9 @@ public:
 
 		m_window = NULL;
 		m_screenSurface = NULL;
+		m_renderer = NULL;
+
+		m_font = NULL;
 	}
 
 	int createWindow(std::string sWindowTitle)
@@ -38,25 +44,42 @@ public:
 		}
 		else
 		{
+
 			//Create window
 			SDL_WindowFlags flags = SDL_WINDOW_OPENGL;
 			m_window = SDL_CreateWindow(sWindowTitle.c_str(), m_nScreenWidth, m_nScreenHeight, flags);
 
-			if (m_window == NULL) printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+			if (m_window == NULL) 
+			{
+				printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+				return 2;
+			}
 
 			else
 			{
-				//Get window surface
-				m_screenSurface = SDL_GetWindowSurface(m_window);
+				//Create renderer for window
+				m_renderer = SDL_CreateRenderer(m_window, NULL);
 
-				//Fill the surface white
-				SDL_FillSurfaceRect(m_screenSurface, NULL, SDL_MapRGB(SDL_GetPixelFormatDetails(m_screenSurface->format), NULL, 0xFF, 0xFF, 0xFF));
+				SDL_SetRenderDrawBlendMode(m_renderer, SDL_BLENDMODE_BLEND);
+				if (m_renderer == NULL)
+				{
+					printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+					return 3;
+				}
+				else
+				{
+					//Initialize renderer color
+					SDL_SetRenderDrawColor(m_renderer, 0x00, 0xFF, 0x00, 0xFF);
+					SDL_Color textColor = { 0, 0, 0, 255 };
 
-				//Update the surface
-				SDL_UpdateWindowSurface(m_window);
-
-				//Hack to get window to stay up
-				SDL_Event e; bool quit = false; while (quit == false) { while (SDL_PollEvent(&e)) { if (e.type == SDL_EVENT_QUIT) quit = true; } }
+					//Initialize SDL_ttf
+					if (TTF_Init() == -1)
+					{
+						printf("SDL_ttf could not initialize! SDL_ttf Error: %s\n", SDL_GetError());
+						m_font = TTF_OpenFont("res/fonts/PixeloidSans-mLxMm.ttf", 50);
+						return 4;
+					}
+				}
 			}
 
 		}
@@ -82,8 +105,11 @@ private:
 	int m_nScreenWidth;
 	int m_nScreenHeight;
 
-	SDL_Window* m_window = NULL;
-	SDL_Surface* m_screenSurface = NULL;
+	SDL_Window* m_window;
+	SDL_Surface* m_screenSurface;
+	SDL_Renderer* m_renderer;
+
+	TTF_Font* m_font;
 
 };
 
