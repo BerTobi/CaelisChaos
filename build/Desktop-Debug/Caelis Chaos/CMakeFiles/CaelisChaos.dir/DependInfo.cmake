047c7486f89434365ce9841fe3a713393e22f061
gustipardo <gusm2003@gmail.com>
2025-07-17 16:26:00 +1200

    Cmake port to linux

---
+++ build/Desktop-Debug/Caelis Chaos/CMakeFiles/CaelisChaos.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/gustavo/Desktop/Projects/CaelisChaos/CaelisChaos/Caelis Chaos/src/CaelisEngine/GUI/Button.cpp" "Caelis Chaos/CMakeFiles/CaelisChaos.dir/src/CaelisEngine/GUI/Button.cpp.o" "gcc" "Caelis Chaos/CMakeFiles/CaelisChaos.dir/src/CaelisEngine/GUI/Button.cpp.o.d"
+  "/home/gustavo/Desktop/Projects/CaelisChaos/CaelisChaos/Caelis Chaos/src/CaelisEngine/GameStates/GameState.cpp" "Caelis Chaos/CMakeFiles/CaelisChaos.dir/src/CaelisEngine/GameStates/GameState.cpp.o" "gcc" "Caelis Chaos/CMakeFiles/CaelisChaos.dir/src/CaelisEngine/GameStates/GameState.cpp.o.d"
+  "/home/gustavo/Desktop/Projects/CaelisChaos/CaelisChaos/Caelis Chaos/src/CaelisEngine/GameStates/MainMenu.cpp" "Caelis Chaos/CMakeFiles/CaelisChaos.dir/src/CaelisEngine/GameStates/MainMenu.cpp.o" "gcc" "Caelis Chaos/CMakeFiles/CaelisChaos.dir/src/CaelisEngine/GameStates/MainMenu.cpp.o.d"
+  "/home/gustavo/Desktop/Projects/CaelisChaos/CaelisChaos/Caelis Chaos/src/CaelisEngine/GameStates/SingleplayerLobby.cpp" "Caelis Chaos/CMakeFiles/CaelisChaos.dir/src/CaelisEngine/GameStates/SingleplayerLobby.cpp.o" "gcc" "Caelis Chaos/CMakeFiles/CaelisChaos.dir/src/CaelisEngine/GameStates/SingleplayerLobby.cpp.o.d"
+  "/home/gustavo/Desktop/Projects/CaelisChaos/CaelisChaos/Caelis Chaos/src/Caelis Chaos.cpp" "Caelis Chaos/CMakeFiles/CaelisChaos.dir/src/Caelis_Chaos.cpp.o" "gcc" "Caelis Chaos/CMakeFiles/CaelisChaos.dir/src/Caelis_Chaos.cpp.o.d"
+  "" "/home/gustavo/Desktop/Projects/CaelisChaos/CaelisChaos/Linux/Debug/CaelisChaos" "gcc" "Caelis Chaos/CMakeFiles/CaelisChaos.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/Desktop-Debug/Caelis Chaos/CMakeFiles/CaelisChaos.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "/home/gustavo/Desktop/Projects/CaelisChaos/CaelisChaos/Linux/Debug/CaelisChaos"
+  "/home/gustavo/Desktop/Projects/CaelisChaos/CaelisChaos/Linux/Debug/CaelisChaos.pdb"
+  "CMakeFiles/CaelisChaos.dir/link.d"
+  "CMakeFiles/CaelisChaos.dir/src/CaelisEngine/GUI/Button.cpp.o"
+  "CMakeFiles/CaelisChaos.dir/src/CaelisEngine/GUI/Button.cpp.o.d"
+  "CMakeFiles/CaelisChaos.dir/src/CaelisEngine/GameStates/GameState.cpp.o"
+  "CMakeFiles/CaelisChaos.dir/src/CaelisEngine/GameStates/GameState.cpp.o.d"
+  "CMakeFiles/CaelisChaos.dir/src/CaelisEngine/GameStates/MainMenu.cpp.o"
+  "CMakeFiles/CaelisChaos.dir/src/CaelisEngine/GameStates/MainMenu.cpp.o.d"
+  "CMakeFiles/CaelisChaos.dir/src/CaelisEngine/GameStates/SingleplayerLobby.cpp.o"
+  "CMakeFiles/CaelisChaos.dir/src/CaelisEngine/GameStates/SingleplayerLobby.cpp.o.d"
+  "CMakeFiles/CaelisChaos.dir/src/Caelis_Chaos.cpp.o"
+  "CMakeFiles/CaelisChaos.dir/src/Caelis_Chaos.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CaelisChaos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
